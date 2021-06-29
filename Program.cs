9eb78ffbffd7fcdc9cc3ea306dfcb1ff957afbb7
Илья Аникин <mercury2612198@gmail.com>
2021-06-29 22:58:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab290621
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arrOne = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] arrTwo = new int[] { 1, 11, 2, 23, 3, 0, 12, 15, 100 };
+            int[] arrThree = new int[] { 2, 3, 2, 2, 4 };
+            Console.WriteLine("ЁБАНЫЙ РОТ ЭТО КАЗИНО БЛЯТЬ!\n");
+            Task Sol = new Task();
+            foreach (int i in Sol.GetMergeArray(arrOne, arrTwo))
+            {
+                Console.Write($" |{i}|");
+            }
+            Console.WriteLine($"\n\n {Sol.GetSum(arrOne)}");
+            Console.WriteLine($"\n {Sol.GetCount(arrThree)}");
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab290621
+{
+    class Task
+    {
+        public int[] GetMergeArray(int[] arrOne, int[] arrTwo) => arrOne.Union(arrTwo).ToArray();
+
+        public int GetSum(int[] arr) 
+        {
+            int sum = 0;            
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public int GetCount(int[] arr)
+        {
+            int count = 0;
+            for(int i = 0; i<arr.Length; i++)
+            {
+                for (int j = 0+i; j < arr.Length; j++)
+                {
+                    if (j>i && arr[i] - arr[j] == 0 || arr[i] + arr[j] == 0) 
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
